e8588cc9c0818db065154c8283af986bca5abfb1
Renny Fadoju <rennyvbirds@yahoo.com>
2020-06-04 10:02:43 +0100

    Add Academy Database Context

--- AcademyResidentInformationApi/V1/Infrastructure/AcademyContext.cs
+++ AcademyResidentInformationApi/V1/Infrastructure/AcademyContext.cs
@@ -16,7 +16,11 @@ namespace AcademyResidentInformationApi.V1.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Address>()
-                .HasNoKey();
+                .HasKey(address => new
+                {
+                    address.ClaimId,
+                    address.HouseId
+                });
         }
 
         //public DbSet<TelephoneNumber> TelephoneNumbers { get; set; }

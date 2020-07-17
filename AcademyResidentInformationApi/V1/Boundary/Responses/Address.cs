b1415ad2a762c565934786e29c7bc607596f0a6b
elcorbs <emma@madetech.com>
2020-07-17 11:09:34 +0100

    Postcode response key is one word

--- AcademyResidentInformationApi/V1/Boundary/Responses/Address.cs
+++ AcademyResidentInformationApi/V1/Boundary/Responses/Address.cs
@@ -6,6 +6,6 @@ namespace AcademyResidentInformationApi.V1.Boundary.Responses
         public string AddressLine2 { get; set; }
         public string AddressLine3 { get; set; }
         public string AddressLine4 { get; set; }
-        public string PostCode { get; set; }
+        public string Postcode { get; set; }
     }
 }
